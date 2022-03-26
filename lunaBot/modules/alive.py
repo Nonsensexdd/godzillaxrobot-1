1b8796b7e7a7e008c6ac6ac4304caf12c3c9834f
Nonsensexdop <100387528+Nonsensexdop@users.noreply.github.com>
2022-03-26 13:38:46 +0530

    Update alive.py

--- lunaBot/modules/alive.py
+++ lunaBot/modules/alive.py
@@ -7,18 +7,18 @@ from pyrogram import __version__ as pyrover
 from lunaBot.events import register as MEMEK
 from lunaBot import telethn as tbot
 
-PHOTO = "https://telegra.ph/file/e57fffe4b3f6189311c23.jpg"
+PHOTO = "https://telegra.ph/file/80e48da9cacee51197bf1.jpg "
 
 @MEMEK(pattern=("/alive"))
 async def awake(event):
   tai = event.sender.first_name
-  xxx = "**❄𝗛𝗼𝗶𝗶, 𝗜 𝗮𝗺 ⏤‌★GODZILLA☆「🇮🇳」𝗥𝗼𝗯𝗼𝘁!!❄ !** \n\n"
+  xxx = "**❄𝗛𝗼𝗶𝗶, 𝗜 𝗮𝗺 ⏤‌★OMFO☆「🇮🇳」𝗥𝗼𝗯𝗼𝘁!!❄ !** \n\n"
   xxx += "🔴 **❄ ✘ 𝗜'𝗺 𝗪𝗼𝗿𝗸𝗶𝗻𝗴 𝗶𝗻 𝗪𝗲𝗹𝗹 𝗠𝗮𝗻𝗻𝗲𝗿!** \n\n"
-  xxx += "🔴 ** ❄ ✘ 𝗛𝘂𝗶 𝗛𝘂𝗶 𝗠𝘆 𝗠𝗮𝘀𝘁𝗲𝗿 : [AJAY](https://t.me/XXXTENTACION_FOREVER)** \n\n"
+  xxx += "🔴 ** ❄ ✘ 𝗛𝘂𝗶 𝗛𝘂𝗶 𝗠𝘆 𝗠𝗮𝘀𝘁𝗲𝗿 : [OMFO](https://t.me/XXXTENTACION_FOREVER)** \n\n"
   xxx += f"🔴 ** ❄ ✘ 𝗧𝗲𝗹𝗲𝘁𝗵𝗼𝗻 𝗩𝗲𝗿𝘀𝗶𝗼𝗻: {tlhver}** \n\n"
   xxx += f"🔴 **❄ ✘ 𝗣𝘆𝗿𝗼𝗴𝗿𝗮𝗺 𝗩𝗲𝗿𝘀𝗶𝗼𝗻 : {pyrover}** \n\n"
   xxx += "**❄「𝗧𝗵𝗮𝗻𝘅𝘅 𝗙𝗼𝗿 𝗨𝘀𝗶𝗻𝗴 𝗠𝗲 𝘀𝘂𝗿」❤️❄**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/godzilla_x_robot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/godzilla_chatting")]]
+  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/omfo_robot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/omfo_chatting")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=xxx,  buttons=BUTTON)
 
 @MEMEK(pattern=("/reload"))
