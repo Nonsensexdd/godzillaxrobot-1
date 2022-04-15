42f0371788ef5c6368fb34a62091d5e0513c66bd
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-04-15 15:01:12 +0530

    Update alive.py

--- lunaBot/modules/alive.py
+++ lunaBot/modules/alive.py
@@ -13,12 +13,12 @@ PHOTO = "https://telegra.ph/file/ba62d1cd0445adea68cde.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Godzilla Robot.** \n\n"
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Omfo Robot.** \n\n"
   TEXT += "⚪ **I'm Working Properly** \n\n"
   TEXT += f"⚪ **My Master : [Xxx](https://t.me/XXXTENTACION_FOREVER)** \n\n"
   TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
   TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
   TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
   TEXT += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("Help", "https://t.me/GODZILLA_X_ROBOT?start=help"), Button.url("Support", "https://t.me/godzilla_chatting")]]
+  BUTTON = [[Button.url("Help", "https://t.me/omfo_robot?start=help"), Button.url("Support", "https://t.me/OMFO_CHATTING")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
